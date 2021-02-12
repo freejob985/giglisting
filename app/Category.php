bbdd612ee400b3101bf78ae040cd29be24fd2a1a
freejob985 <freejob985@gmail.com>
2021-02-12 22:03:08 +0200

    asd

--- app/Category.php
+++ app/Category.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Support\Facades\DB;
+use Session;
 
 class Category extends Model
 {
