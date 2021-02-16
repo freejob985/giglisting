e08bcdd0de1ce5c83d25e887b688f58abb9b5f6b
freejob985 <freejob985@gmail.com>
2021-02-16 07:45:32 +0200

    asd

--- resources/views/frontend/partials/nav.blade.php
+++ resources/views/frontend/partials/nav.blade.php
@@ -7,7 +7,8 @@
     <div class="site-mobile-menu-body"></div>
 </div>
 
-<header class="site-navbar container py-0 bg-white customization-header-background-color" role="banner" style="max-width:100%!important;top:0;">
+<header class="site-navbar container py-0 bg-white customization-header-background-color" role="banner"
+    style="max-width:100%!important;top:0;">
 
     <!-- <div class="container"> -->
     <div class="row align-items-center">
@@ -45,99 +46,104 @@
             <nav class="site-navigation position-relative text-right" role="navigation">
 
                 <ul class="site-menu js-clone-nav mr-auto d-none d-lg-block pl-4">
-                                     	<li>
-	<a href="https://gigworldgo.com"> Home</a>
-	</li>
-	<li class="menu-item-has-children has-children">
-	<a href="https://gigworldgo.com/about"> About Us</a>
-	<ul class="sub-menu dropdown">
-	<li>
-	<a href="https://gigworldgo.com/about">About Us</a>
-	</li>
-	<li>
-	<a href="https://gigworldgo.com/p/78/why-gig222">Why GIG?</a>
-	</li>
-	<li>
-	<a href="https://gigworldgo.com/p/79/the-features-and-advantages-that-gig-brings-to-you222222">The features and advantages that GIG brings to you</a>
-	</li>
-	<li>
-	<a href="https://gigworldgo.com/p/80/our-services2">Our Services</a>
-	</li>
-	<li>
-	<a href="https://gigworldgo.com/team">Team</a>
-	</li>
-	<li>
-	<a href="https://gigworldgo.com/career">Career With Us</a>
-	</li>
-	</ul>
-	</li>
-	<li class="menu-item-has-children has-children">
-	<a href="https://gigworldgo.com/service"> Service</a>
-	<ul class="sub-menu dropdown">
-	<li>
-	<a href="/gigs/category/1/educational-services">Educational Services</a>
-	</li>
-	<li>
-	<a href="/gigs/category/2/export-services-and-import">Export Services And Import</a>
-	</li>
-	<li>
-	<a href="/gigs/category/3/legal-services">Legal Services</a>
-	</li>
-	<li>
-	<a href="/gigs/category/4/real-estate">Real Estate</a>
-	</li>
-	<li>
-	<a href="/gigs/category/5/restaurant-requests">Restaurant Requests</a>
-	</li>
-	<li>
-	<a href="/gigs/category/6/accounting-and-legal-audit-services">Accounting And Legal Audit Services</a>
-	</li>
-	<li>
-	<a href="/gigs/category/7/livestock-development">Livestock Development</a>
-	</li>
-	<li>
-	<a href="/gigs/category/29/agricultural-investment-services">Agricultural Investment Services</a>
-	</li>
-	<li>
-	<a href="/gigs/category/31/human-development">Human Development</a>
-	</li>
-	<li>
-	<a href="/gigs/category/30/advertising-services">Advertising Services</a>
-	</li>
-	<li>
-	<a href="/gigs/category/32/marriage-service">Marriage Service</a>
-	</li>
-	<li>
-	<a href="/gigs/category/33/visa-services">Visa Services</a>
-	</li>
-	<li>
-	<a href="https://gigworldgo.com/events">Events</a>
-	</li>
-	</ul>
-	</li>
-	<li>
-	<a href="https://gigworldgo.com/our-works"> Our Works</a>
-	</li>
-	<li class="menu-item-has-children has-children">
-	<a href="http://ourstore.gigworldgo.com/"> Our Store</a>
-	<ul class="sub-menu dropdown">
-	<li>
-	<a href="http://ourstore.gigworldgo.com/">Our Products</a>
-	</li>
-	<li>
-	<a href="http://ourstore.gigworldgo.com/categories">Our categories</a>
-	</li>
-	<li>
-	<a href="https://gigworldgo.com/login/admin">Join as seller</a>
-	</li>
-	</ul>
-	</li>
-	<li>
-	<a href="https://gigworldgo.com/blog"> Blog</a>
-	</li>
-	<li>
-	<a href="https://gigworldgo.com/contact"> {{ \App\Setting::lang("Contact",Session::get('lang')) }}</a>
-	</li>
+                    <li>
+                        <a href="https://gigworldgo.com">{{ \App\Setting::lang("Home",Session::get('lang')) }} </a>
+                    </li>
+                    <li class="menu-item-has-children has-children">
+                        <a href="https://gigworldgo.com/about">{{ \App\Setting::lang("About Us",Session::get('lang')) }} </a>
+                        <ul class="sub-menu dropdown">
+                            <li>
+                                <a href="https://gigworldgo.com/about">{{ \App\Setting::lang("About Us",Session::get('lang')) }}</a>
+                            </li>
+                            <li>
+                                <a href="https://gigworldgo.com/p/78/why-gig222">{{ \App\Setting::lang("Why GIG?",Session::get('lang')) }}</a>
+                            </li>
+                            <li>
+                                <a
+                                    href="https://gigworldgo.com/p/79/the-features-and-advantages-that-gig-brings-to-you222222">{{ \App\Setting::lang("The
+                                    features and advantages that GIG brings to you",Session::get('lang')) }}</a>
+                            </li>
+                            <li>
+                                <a href="https://gigworldgo.com/p/80/our-services2">Our Services</a>
+                            </li>
+                            <li>
+                                <a href="https://gigworldgo.com/team">{{ \App\Setting::lang("Team",Session::get('lang')) }}</a>
+                            </li>
+                            <li>
+                                <a href="https://gigworldgo.com/career">{{ \App\Setting::lang("Career With Us",Session::get('lang')) }}</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="menu-item-has-children has-children">
+                        <a href="https://gigworldgo.com/service">{{ \App\Setting::lang("Service",Session::get('lang')) }} </a>
+                        <ul class="sub-menu dropdown">
+                            <li>
+                                <a href="/gigs/category/1/educational-services">{{ \App\Setting::lang("Educational Services",Session::get('lang')) }}</a>
+                            </li>
+                            <li>
+                                <a href="/gigs/category/2/export-services-and-import">{{ \App\Setting::lang("Export Services And Import",Session::get('lang')) }}</a>
+                            </li>
+                            <li>
+                                <a href="/gigs/category/3/legal-services">{{ \App\Setting::lang("Legal Services",Session::get('lang')) }}</a>
+                            </li>
+                            <li>
+                                <a href="/gigs/category/4/real-estate">{{ \App\Setting::lang("Real Estate",Session::get('lang')) }}</a>
+                            </li>
+                            <li>
+                                <a href="/gigs/category/5/restaurant-requests">{{ \App\Setting::lang("Restaurant Requests",Session::get('lang')) }}</a>
+                            </li>
+                            <li>
+                                <a href="/gigs/category/6/accounting-and-legal-audit-services">{{ \App\Setting::lang("Accounting And Legal
+                                    Audit Services",Session::get('lang')) }}</a>
+                            </li>
+                            <li>
+                                <a href="/gigs/category/7/livestock-development">{{ \App\Setting::lang("Livestock Development",Session::get('lang')) }}</a>
+                            </li>
+                            <li>
+                                <a href="/gigs/category/29/agricultural-investment-services">{{ \App\Setting::lang("Agricultural Investment
+                                    Services",Session::get('lang')) }}</a>
+                            </li>
+                            <li>
+                                <a href="/gigs/category/31/human-development">{{ \App\Setting::lang("Human Development",Session::get('lang')) }}</a>
+                            </li>
+                            <li>
+                                <a href="/gigs/category/30/advertising-services">{{ \App\Setting::lang("Advertising Services",Session::get('lang')) }}</a>
+                            </li>
+                            <li>
+                                <a href="/gigs/category/32/marriage-service">{{ \App\Setting::lang("Marriage Service",Session::get('lang')) }}</a>
+                            </li>
+                            <li>
+                                <a href="/gigs/category/33/visa-services">{{ \App\Setting::lang("Visa Services",Session::get('lang')) }}</a>
+                            </li>
+                            <li>
+                                <a href="https://gigworldgo.com/events">{{ \App\Setting::lang("Events",Session::get('lang')) }}</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="https://gigworldgo.com/our-works">{{ \App\Setting::lang("Our Works",Session::get('lang')) }} </a>
+                    </li>
+                    <li class="menu-item-has-children has-children">
+                        <a href="http://ourstore.gigworldgo.com/">{{ \App\Setting::lang("Our Store",Session::get('lang')) }} </a>
+                        <ul class="sub-menu dropdown">
+                            <li>
+                                <a href="http://ourstore.gigworldgo.com/">{{ \App\Setting::lang("Our Products",Session::get('lang')) }}</a>
+                            </li>
+                            <li>
+                                <a href="http://ourstore.gigworldgo.com/categories">{{ \App\Setting::lang("Our categories",Session::get('lang')) }}</a>
+                            </li>
+                            <li>
+                                <a href="https://gigworldgo.com/login/admin">{{ \App\Setting::lang("Join as seller",Session::get('lang')) }}</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="https://gigworldgo.com/blog">{{ \App\Setting::lang("Blog",Session::get('lang')) }} </a>
+                    </li>
+                    <li>
+                        <a href="https://gigworldgo.com/contact">
+                            {{ \App\Setting::lang("Contact",Session::get('lang')) }}</a>
+                    </li>
                     @guest
                     <li class="ml-xl-3 login"><a href="{{ route('login') }}"><span
                                 class="border-left pl-xl-4"></span>{{ __('frontend.header.login') }}</a></li>
