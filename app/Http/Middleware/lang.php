a9e0d5a5835a07e54f46cf4d47878ebc772fb335
freejob985 <freejob985@gmail.com>
2021-02-16 03:44:18 +0200

    asd

--- app/Http/Middleware/lang.php
+++ app/Http/Middleware/lang.php
@@ -3,13 +3,17 @@
 namespace App\Http\Middleware;
 
 use Closure;
-
+use Session;
 class lang
 {
 
-    public function handle($request, Closure $next, $role)
+    public function handle($request, Closure $next)
     {
-        dd(Session::get('lang'));
+         if (Session::get('lang') == "") {
+                Session::put('lang', "en");
+                //  dd("Catch errors for script and full tracking ( 1 )");
+            }
+      //  dd(Session::get('lang'));
         return $next($request);
     }
 }
